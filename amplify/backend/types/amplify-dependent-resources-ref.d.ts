3d4d8729f01169014a1877107b6b6fe5ab240980
jongs001 <jongs001@mymail.unisa.edu.au>
2022-09-02 13:35:57 +0930

    auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "driverGroupRole": "string",
+            "customerGroupRole": "string"
+        },
+        "testapp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "testappPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    }
+}
